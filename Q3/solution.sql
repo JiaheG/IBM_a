29db55d6ed562f40a8990cc211af65b2bc0a238a
JiaheG <34421422+JiaheG@users.noreply.github.com>
2023-03-03 17:45:53 +0800

    Update solution.sql

--- Q3/solution.sql
+++ Q3/solution.sql
@@ -1,1 +1,5 @@
-
+select o.owner_id, o.owner_name, count(cam.category_id) different_category_count
+from owner o, article a, category_article_mapping cam
+where a.category_id = cam.category_id and a.owner_id = o.owner_id
+group by  o.owner_id
+order by  different_category_count desc;
